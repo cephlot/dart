311baec704cd66e39e230af689a84a7bfcbf31a7
Albert Gustavsson <albert99gustavsson@gmail.com>
2022-04-25 10:43:23 +0200

    Added timing code to compare performance on raspberry pi

--- DartLocalizationMain.py
+++ DartLocalizationMain.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import cv2
 from DartLocalization import DartLocalization
 
@@ -11,7 +12,11 @@ image_with_dart = cv2.imread("images/board_20.png")
 image_with_dart = cv2.resize(image_with_dart, None, fx=scale, fy=scale, interpolation=cv2.INTER_AREA)
 # cv2.imshow("Board with dart", image_with_dart)
 
+start_time = datetime.now()
 dart_x, dart_y = DartLocalization.find_dart_point(image_without_dart, image_with_dart)
+end_time = datetime.now()
+
+print("Calculation took " + str((end_time-start_time).total_seconds()*1000) + "ms")
 
 cv2.circle(image_with_dart, (int(dart_x), int(dart_y)), radius=10, color=(0,255,0), thickness=2)
 cv2.imshow("Point", image_with_dart)
