996f321ec5392c2f5cbb33ece7493f50ba150696
Simon <c18sum@cd.umu.se>
2022-04-26 10:20:06 +0200

    feat: point mask helper done

--- Helpers/PointMaskHelper.py
+++ Helpers/PointMaskHelper.py
@@ -7,6 +7,8 @@ import numpy as np
     point reagions of the board as these are the most distinguished lines.
     The image must not have lines that are more distinct than the point region lines.
     Parametres
+    Every segment will be filled with a value from 1 to 20, these are NOT scores, only
+    used to differentiate from each region.
     -----------
     image
         the image to generate lines from
@@ -17,9 +19,13 @@ def generate_point_mask(image):
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cann_img = cv2.Canny(image_gray,200,300)
     lines = cv2.HoughLines(cann_img, 1, np.pi/180, 120, np.array([]))
+    # Black out entire image
+    h,w = image.shape[:2]
+    lineImage = np.zeros((h,w,1), dtype = "uint8")
     angle_list = []
     i = 0
     range = 10
+    # index of lines in order to seperate them
     while i < range:
         line = lines[i]
         rho, theta = line[0]
@@ -35,9 +41,51 @@ def generate_point_mask(image):
             range += 1
         else:
             angle_list.append(np.round(theta/np.pi*180))
-            cv2.line(image,(x1,y1),(x2,y2),(0,0,255),2)
+            cv2.line(lineImage,(x1,y1),(x2,y2),(255),1)
         i += 1
-    return image
+    
+    filledImage = fillSegments(lineImage)
+    filledImage = filledImage - lineImage
+    cv2.imwrite('/mnt/c/dart/pics/filledImage.png', filledImage)
+    return filledImage
+
+'''
+    Helper method that fills each segment
+    with a unique colour. Colours range from
+    1 to 20 in grayscale (1 dimension)
+    -----------
+    image
+        Image to fill
+    return
+        Segmented Image
+'''
+def fillSegments(image):
+    image2 = image.copy()
+    h,w = image2.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+    region = 255
+    for x in range(w):
+        # Top Border
+        if(image2[0,x] == 0):
+            cv2.floodFill(image2, mask, (x,0), (region))
+            region -= 10
+        # Bottom Border
+        if(image2[h-1,x] == 0):
+            cv2.floodFill(image2, mask, (x,h-1), (region))
+            region -= 10
+    for y in range(h):
+        # Left Border
+        if(image2[y,0] == 0):
+            cv2.floodFill(image2, mask, (0,y), (region))
+            region -= 10
+        # Right Border
+        if(image2[y,w-1] == 0):
+            cv2.floodFill(image2, mask, (w-1,y), (region))
+            region -= 10
+    return image2
+
+
+
 '''
     Helper method that checks if the angle theta
     is too close to antother already saved angle in list. 
@@ -59,3 +107,7 @@ def exists_line(theta, list):
         if(abs(d) < 10):
             return True
     return False
+
+
+image = cv2.imread('/mnt/c/dart/pics/pic.png') 
+generate_point_mask(image)
