74dfac2529f3a3b637c2da0479fa604d3915e35b
ceph <c18oln@cs.umu.se>
2022-05-10 11:57:19 +0200

    [docs] Updates Dart comments

--- Dart.py
+++ Dart.py
@@ -18,13 +18,21 @@ class Dart:
         #cv.waitKey(0)
 
     def wait(self):
+        '''
+        Wrapper for MotionDetector.wait_for_motion. Stores frames before and 
+        frames after.
+
+        :param self: self object.
+        '''
         frames_before, frames_after = self.detector.wait_for_motion()
         self.frames_before = frames_before
         self.frames_after = frames_after
 
     def get_score(self):
         '''
-        Gets the score from a dart by using evaluator
+        Gets the score from a dart by using evaluator.
+
+        :param self: self object.
         '''
         print("BEFORE")
         print(len(self.frames_before))
@@ -35,15 +43,14 @@ class Dart:
         '''
         Waits for motion to be detected and then determines if there's a change 
         of players.
+
+        :param self: self object.
         '''
 
         condition = False
 
         while condition is False:
             frames_before, frames_after = self.detector.wait_for_motion()
-
-            # analyse image
             condition = ImageAnalyzer.analyze(self.frames_before, frames_after)
 
-        # Set new background
         self.frames_before = frames_after
