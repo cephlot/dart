9db2ab90f7764b346708336e54dfbef4b445e359
Vedad Coric <c18vcc@cs.umu.se>
2022-05-10 11:32:14 +0200

    Producing hotfix for dart localization

---
+++ dartTest.py
@@ -0,0 +1,15 @@
+import cv2
+from DartLocalization import DartLocalization
+
+image_without_dart = cv2.imread("GoodArrow.jpg")
+
+image_with_dart = cv2.imread("StopidArrow.jpg")
+
+dart_x, dart_y = DartLocalization.find_dart_point(image_without_dart, image_with_dart)
+
+cv2.circle(image_with_dart, (int(dart_x), int(dart_y)), radius=10, color=(0,255,0), thickness=2)
+cv2.imshow("Point", image_with_dart)
+
+cv2.waitKey()
+
+cv2.destroyAllWindows()
