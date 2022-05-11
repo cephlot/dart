1955fcc7242db32a9fd00600a47898ed18bf349c
Simon <c18sum@cd.umu.se>
2022-05-11 13:01:32 +0200

    feat - ScoreEvaluator restored

--- ScoreEvaluator.py
+++ ScoreEvaluator.py
@@ -1,5 +1,4 @@
 
-from warnings import catch_warnings
 from cv2 import imshow
 from CoordinateProjector import CoordinateProjector
 from DartLocalization import DartLocalization
@@ -25,7 +24,6 @@ class ScoreEvaluator:
     "TODO: Make evaluate only generate new transformation matrix when new player by taking boolean parameter"
     def evaluate(self, image_B_frames, image_I_frames):
         """Uses the RegionSegmenter and dart localizer to score
-
         :param image_B_frames: images from all cameras before dart is thrown
         :type image_B_frames: list
         :param image_I_frames: images from all cameras after dart is thrown
@@ -39,26 +37,26 @@ class ScoreEvaluator:
         coordinate_list         =  self.get_dart_coordinates(image_B_frames, image_I_frames)
         coordinate_list         =  self.check_if_no_dart(coordinate_list)
         proj_coordinate_list    =  self.project_coordinates(coordinate_list)
+        if(len(proj_coordinate_list) == 0):
+            print("ERROR: No good projeciton where possible, returning score 0")
+            return 0
         proj_coordinate_list    =  self.quality_control_projected_coordinates(proj_coordinate_list)
         average_coordinate      =  self.average_coordinates(proj_coordinate_list)
         if(average_coordinate is None):
             return 0
         
-        image_ref = cv.imread('images\pic_nice.jpg')
-
+        '''image_ref = cv.imread('images\pic_nice.jpg')
+        test = image_ref
         for _,proj in enumerate(proj_coordinate_list):
-            test = cv.circle(image_ref, proj, 10, (250, 0, 0), 4)
-            cv.imshow("yes", test)
-            cv.waitKey(0)
-        test = cv.circle(image_ref, average_coordinate, 10, (0, 250, 0), 4)
+            test = cv.circle(test, proj, 10, (250, 0, 0), 4)
+        test = cv.circle(test, average_coordinate, 8, (0, 250, 0), 3)
         cv.imshow("yes", test)
-        cv.waitKey(0)
+        cv.waitKey(0)'''
         return self.score_coordinate(average_coordinate)
 
 
     def score_coordinate(self, coordinate):
         """Scors the average dart coordinate using scoring mask
-
         :param coordinate: coordinate to compute from
         :type coordinate: list
         :return: score 
@@ -75,7 +73,6 @@ class ScoreEvaluator:
 
     def error_check_image_length(self, image_B_frames,image_I_frames):
         """Checks the length of image lists
-
         :param image_B_frames: list of images before
         :type image_B_frames: list
         :param image_I_frames: list of images after
@@ -93,7 +90,6 @@ class ScoreEvaluator:
         """ Returns all dart coordinate estimates found by DartLocalization using all cameras before and after images
         Coordinates returned are PredictedCoordinate objects. this is done to keep track of
         what coordinates are from what camera even after deleting some bad coordinates
-
         :param image_B_frames: list of images before dart is thrown
         :type image_B_frames: list
         :param image_I_frames: list of images after dart is thrown
@@ -105,14 +101,13 @@ class ScoreEvaluator:
         coordinate_list = []
         for i, _ in enumerate(image_B_frames):
             x, y = DartLocalization.find_dart_point(image_B_frames[i], image_I_frames[i])
-            pc = PredictedCoordinate(x,y,i)
-            coordinate_list.append(pc)
+            if(not (x < 0 and y < 0)):
+                pc = PredictedCoordinate(x,y,i)
+                coordinate_list.append(pc)
         return coordinate_list
 
     def create_projectors(self, image_B_frames):
         """Creates projectors and their projection matrix for all cameras 
-
-
         :param image_B_frames: images before dart is thrown
         :type image_B_frames: list
         :return: projectors
@@ -128,23 +123,19 @@ class ScoreEvaluator:
 
     def check_if_no_dart(self, coordinate_list):
         """Removes darrts from coordinate list that were not found
-
         :param coordinate_list: list of coordinates
         :type coordinate_list: list
         :return: coordinate list
         :rtype: list
         """
-        
-        for i, c in enumerate(coordinate_list):
-            if c.get_x() < 0 and c.get_y() < 0:
+        for c in coordinate_list:
+            if c.get_x() < 1 and c.get_y() < 1:
                 print(f'No dart ({c.get_x()},{c.get_y()})')
-                del coordinate_list[i]
+                coordinate_list.remove(c)
         return coordinate_list
 
     def project_coordinates(self, coordinate_list):
         """Projects all dart coordinates from DartLocalization.find_dart_point using the project_dart_coordinate method
-
-
         :param coordinate_list: coordinate list
         :type coordinate_list: list
         :return: projected coordinate list
@@ -152,8 +143,8 @@ class ScoreEvaluator:
         """
 
         proj_coordinate_list = []
-        for i,c in enumerate(coordinate_list):
-            if(not self.projectors[i] is None):
+        for c in coordinate_list:
+            if(self.projectors[c.get_camera_index()].hasMatrix()):
                 proj_coordinate_list.append(self.projectors[c.get_camera_index()].project_dart_coordinate((c.get_x(), c.get_y()))) 
         return proj_coordinate_list
 
@@ -164,7 +155,6 @@ class ScoreEvaluator:
             Distance from average coordinate is 1.3X further away than average distance from average coordinate
             Distance from average coordinate is at least 5 pixels
             Coordinate is furthest away
-
         :param proj_coordinate_list: projected coordinates list
         :type proj_coordinate_list: list
         :return: filtered list
@@ -172,9 +162,7 @@ class ScoreEvaluator:
         """
 
         proj = self.check_negative_projection(proj_coordinate_list)
-        if(len(proj) == 0):
-            return None
-        elif(len(proj) != 3):
+        if(len(proj) != 3):
             return proj
         average = self.average_coordinates(proj)
         avarage_distance = 0
@@ -182,7 +170,7 @@ class ScoreEvaluator:
             avarage_distance += self.distance(average, c)
         avarage_distance = avarage_distance / len(proj)
         worst_c = None
-        for _, c in enumerate(proj):
+        for c in proj:
             distanceFromAvarage = self.distance(average, c)
             if(distanceFromAvarage > avarage_distance*1.1 and distanceFromAvarage > self.PIXEL_FACTOR):
                 if(worst_c is None):
@@ -196,7 +184,6 @@ class ScoreEvaluator:
     
     def check_negative_projection(self,coordinates):
         """Checks for negative projections and filters them out
-
         :param coordinates: coordinate list
         :type coordinates: list
         :return: filtered list
@@ -204,26 +191,29 @@ class ScoreEvaluator:
         """
 
         for c in coordinates:
-            if(c[0] < 0 or c[1] < 0):
+            if(c[0] < 1 or c[1] < 1):
                 coordinates.remove(c)
+                print(f"check_negative_projection -- ERROR -- Projected coordinate for dart out of bounce {c}")
             if(c[0] > self.reference.shape[0] or c[1] > self.reference.shape[1]):
+                print(f"check_negative_projection -- ERROR -- Projected coordinate for dart out of bounce {c}")
                 coordinates.remove(c)
         return coordinates
 
     def average_coordinates(self, coordinates):
         """Calculates the average coordinates
-
         :param coordinates: coordinate list
         :type coordinates: list
         :return: average coordinates
         :rtype: (int,int)
         """
+        if(len(coordinates) == 0):
+            print("average_coordinates -- ERROR -- No projected coordinate found to average -- returning (0,0)")
+            return None
         c = [sum(x)/len(x) for x in zip(*coordinates)]
         return ((round(c[0])),round(c[1]))
 
     def distance(self, c1, c2):
         """Finds distance between two coordinates
-
         :param c1: first coordinate
         :type c1: (int,int)
         :param c2: second coordinate
