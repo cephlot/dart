944398d1cb20fd5e8bb24f482c9946bea9fc489f
Simon <c18sum@cd.umu.se>
2022-05-18 12:43:10 +0200

    [feat] new print

--- CameraSetup.py
+++ CameraSetup.py
@@ -39,7 +39,6 @@ class cameraSetup:
             if i is not exception:
                 if cameraSetup.testDevice(i) == 0:
                     valid_cams.append(i)
-                else:
                     print(" --- Able to open camera ", i, " --- ")
         return valid_cams
 
