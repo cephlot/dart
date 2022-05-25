68017394b43ea2f67a5a89b2e3fabdc08c0b7dfa
ceph <c18oln@cs.umu.se>
2022-05-25 14:19:23 +0200

    [fix] Coord fix

--- GameMode.py
+++ GameMode.py
@@ -77,7 +77,7 @@ class GameMode301(GameMode):
 			self.game_status = GameStatus.GET_DARTS
 
 		Requester.post_scores(self.scores, self.current_player)
-		Requester.post_coords(coords[0], coords[0])
+		Requester.post_coords(coords[0], coords[1])
 
 	def change_player(self):
 		self.current_player = (self.current_player + 1) % self.player_count
